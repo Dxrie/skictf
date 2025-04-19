c2087c59693d54082e9660901ca039921d2914d7
Dxrie <colinedric04@gmail.com>
2025-04-19 16:51:45 +0700

    idk

--- app/api/team/all/route.ts
+++ app/api/team/all/route.ts
@@ -1,8 +1,10 @@
 import connectDB from "@/lib/db";
 import Team from "@/models/Team";
+import { User } from "@/models/User";
 
 export async function GET() {
   await connectDB();
+
   const teams = await Team.find({ showInLeaderboard: true })
     .populate("members", "username _id")
     .sort({
