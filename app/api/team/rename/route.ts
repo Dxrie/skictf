ed15888a845161299915095885b33909965172e1
Dxrie <colinedric04@gmail.com>
2025-04-29 10:18:28 +0700

    update bug

--- app/api/team/rename/route.ts
+++ app/api/team/rename/route.ts
@@ -1,5 +1,6 @@
 import authOptions from "@/app/authOptions";
 import Team from "@/models/Team";
+import { User } from "@/models/User";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -14,7 +15,13 @@ export async function PUT(request: NextRequest) {
     const body = await request.json();
     const { name } = body;
 
-    const team = await Team.findById(session.user.teamId);
+    const user = await User.findById(session.user.id);
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const team = await Team.findById(user.teamId);
 
     if (!team) {
       return NextResponse.json({ message: "Team not found" }, { status: 404 });
