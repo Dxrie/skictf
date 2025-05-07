a55432f6e7d0fbcd1cb020e85fa2301edf1e34c0
Dxrie <colinedric04@gmail.com>
2025-05-07 10:41:52 +0700

    keep db alive

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 if (!process.env.MONGODB_URI) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env"
+    "Please define the MONGODB_URI environment variable inside .env",
   );
 }
 
@@ -14,7 +14,7 @@ let cached: {
 } = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = {conn: null, promise: null};
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
@@ -25,11 +25,18 @@ async function connectDB() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+      maxPoolSize: 10, // max number of connections in the pool
+      minPoolSize: 2, // keep some idle connections ready
+      maxIdleTimeMS: 1000 * 60, // close idle connections after 1 minute
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      keepAlive: true,
+      keepAliveInitialDelay: 300000, // 5 minutes
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => mongoose);
   }
 
   try {
